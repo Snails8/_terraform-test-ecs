493c2b19226fc405dc1f731f3d6cc77af96f7ff9
S-tanishi <tanichi.u.n.20@gmail.com>
2021-10-22 17:05:54 +0900

    一旦コミット

--- elb/main.tf
+++ elb/main.tf
@@ -1,14 +1,3 @@
-# ===========================================================================
-# ELB の設定 AWS Elastic Load Balancing
-#
-# 1. ALBの作成
-# 1-1. httpとhttpsの通信を受け取れるように
-# 2. 通信を行うためにALBのリスナー設定( http => httpsに流す )
-# 3. ECSのnginxに流してやる(処理の依存関係上ココでは行わないのでoutput)
-
-# ACM ECS と依存関係
-# ===========================================================================
-
 # ========================================================================
 # ALB 作成 (+ subnets , security Group 紐付け)
 # https://docs.aws.amazon.com/ja_jp/elasticloadbalancing/latest/application/introduction.html
@@ -57,19 +46,6 @@ resource "aws_security_group_rule" "http" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
-# ALB用セキュリティグループへhttpsも受け付けるようルールを追加する
-resource "aws_security_group_rule" "https" {
-  security_group_id = aws_security_group.main.id
-
-  type = "ingress"
-
-  from_port = 443
-  to_port   = 443
-  protocol  = "tcp"
-
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
 # ============================================================
 # 接続リクエストのLBの設定(リスナーの追加) (HTTP)
 # 
@@ -92,25 +68,6 @@ resource "aws_lb_listener" "http" {
       port        = 443
       protocol    = "HTTPS"
       status_code = "HTTP_301"
-    }
-  }
-}
-# https 
-resource "aws_lb_listener" "https" {
-  port     = 443
-  protocol = "HTTPS"
-
-  # TSL証明書等を受け取る(処理はacm)
-  # certificate_arn   = var.acm_id
-  load_balancer_arn = aws_lb.main.arn
-  # "ok" という固定レスポンスを設定する
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      status_code  = "200"
-      message_body = "ok"
     }
   }
 }
